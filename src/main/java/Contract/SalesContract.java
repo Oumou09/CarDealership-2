bc71a94cbce24c88f71ab5ea6dd75c7fb146c3b1
Oumou09 <oumoudesigns@gmail.com>
2025-05-21 01:39:13 -0400

    Finished creating the attribute for the sales contract and the lease contract

--- src/main/java/Contract/SalesContract.java
+++ src/main/java/Contract/SalesContract.java
@@ -1,32 +1,71 @@
 package Contract;
 
-public class SalesContract {
+import com.pluralsight.dealership.Vehicle;
 
+public class SalesContract extends Contract {
+    private double salesTaxAmount;
+    private double recordingFee;
+    private double processingFee;
+    private boolean finance;
+    private double monthlyPayment;
 
-//    @Override
-//    public double getTotalPrice() {
-//        return getVehicleSold().getPrice() + salesTaxAmount + recordingFee + processingFee;
-//    }
-//
-//    @Override
-//    public double getMonthlyPayment() {
-//        int numberOfPayments = 0;
-//        double interestRate = 0;
-//        if (financeOption) {
-//            if (getVehicleSold().getPrice() >= 10000) {
-//                numberOfPayments = 48;
-//                interestRate = 4.25 / 1200;
-//            } else {
-//                numberOfPayments = 24;
-//                interestRate = 5.25 / 1200;
-//            }
-//
-//            double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
-//            monthlyPayment = Math.round(monthlyPayment * 100);
-//            monthlyPayment /= 100;
-//            return monthlyPayment;
-//        } else {
-//            return 0.0;
-//        }
-//    }
+    public SalesContract(String date, String customerName, String customerEmail, Vehicle vehicleSold, boolean finance) {
+        super(date, customerName, customerEmail, vehicleSold);
+        this.salesTaxAmount = vehicleSold.getPrice() * 0.05;
+        this.recordingFee = 100.00;
+        this.processingFee = (vehicleSold.getPrice() < 10000) ? 295: 495;
+        this.finance = finance;
+    }
+
+    public double getSalesTaxAmount() {
+
+        return salesTaxAmount;
+    }
+
+    public void setSalesTaxAmount(double salesTaxAmount) {
+        this.salesTaxAmount = salesTaxAmount;
+    }
+
+    public double getRecordingFee() {
+        return recordingFee;
+    }
+
+    public void setRecordingFee(double recordingFee) {
+        this.recordingFee = recordingFee;
+    }
+
+    public boolean isFinance() {
+        return finance;
+    }
+
+    public void setFinance(boolean finance) {
+        this.finance = finance;
+    }
+
+        @Override
+    public double getTotalPrice() {
+        return getVehicleSold().getPrice() + salesTaxAmount + recordingFee + processingFee ;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        int numberOfPayments = 0;
+        double interestRate = 0;
+        if (finance) {
+            if (getVehicleSold().getPrice() >= 10000) {
+                numberOfPayments = 48;
+                interestRate = 4.25 / 1200;
+            } else {
+                numberOfPayments = 24;
+                interestRate = 5.25 / 1200;
+            }
+
+            double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+            monthlyPayment = Math.round(monthlyPayment * 100);
+            monthlyPayment /= 100;
+            return monthlyPayment;
+        } else {
+            return 0.0;
+        }
+    }
 }
