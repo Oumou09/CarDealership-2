b750ea322a11ec4ecb15d2f6f6e69305bf21d324
Oumou09 <oumoudesigns@gmail.com>
2025-05-20 23:29:47 -0400

    Finised creating my classes and my story board

---
+++ src/main/java/Contract/ContractDataManager.java
@@ -0,0 +1,6 @@
+package Contract;
+
+public class ContractDataManager {
+
+
+}
---
+++ src/main/java/com/pluralsight/dealership/CarDealership.java
@@ -0,0 +1,17 @@
+package com.pluralsight.dealership;
+
+public class CarDealership {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.display();
+    }
+
+
+
+
+
+
+
+
+
+}
